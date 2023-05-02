607fd22902ef666b44eaf38944f0cb397e6d0133
JefersonCL21 <117442532+JefersonCL21@users.noreply.github.com>
2023-05-02 20:07:19 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+
+    $('#telefone').mask('(00) 00000-000')
+    $('#cpf').mask('000.000.000-00')
+    $('#cep').mask('00.000-000')
+
+    $('form').validate({
+        rules: {
+            nome:{
+                required:true
+            },
+            email:{
+                required:true,
+                email:true
+            },
+            telefone:{
+                required:true
+            },
+            cpf:{
+                required: true
+            },
+            cep:{
+                required:true
+            },
+            endereco:{
+                required:true
+            }
+        },
+        messages:{
+            nome: 'Por favor, insira seu nome'
+        },
+        submitHandler: function(form){
+            console.log(form)
+
+            $('#mensagem').empty().text('Cadastro efetuado!').addClass('mensagem-cadastro');
+            form.reset();
+        },
+        invalidHandler: function(evento, validador){
+            let camposIncorretos = validador.numberOfInvalids();
+            if(camposIncorretos){
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+                form.reset();
+            }
+        }
+    })
+
+})
